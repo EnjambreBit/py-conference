92e180dea6385cebcb3daddecb4a4b6c6c2dd119
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-05-18 00:19:24 -0300

    .

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
     path("api/", include("conferences.urls")),
     path("", include("pages.urls")),
--- pages/views/registration.py
+++ pages/views/registration.py
@@ -4,18 +4,24 @@ from conferences.models.events import Event
 from pages.forms.event_registration import EventRegistrationForm
 from django.contrib.auth.models import User
 
+from django.views.generic import ListView
+
+
+
+class EventRegistrationListView(ListView):
+    model = Event
+    template_name = "registration/available-registration-events.html"
+
+
 
 
 
 def profile_register(request):
-    template = loader.get_template("event-registration.html")
+    template = loader.get_template("registration/event-registration.html")
     if request.method == 'POST':
         form = EventRegistrationForm(request.POST)
         if form.is_valid():
-            
-
-
-            template = loader.get_template("event-registration-susccessful.html")
+            template = loader.get_template("registration/event-registration-susccessful.html")
 
     else:
         form = EventRegistrationForm()
