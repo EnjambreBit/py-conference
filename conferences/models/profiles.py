f864e6dd637dd1beb46c3a240b185d5bf7893dba
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-05-31 20:32:17 -0300

    profile agregado direccion

--- conferences/models/profiles.py
+++ conferences/models/profiles.py
@@ -21,6 +21,7 @@ class Profile(models.Model):
         max_length=100, choices=GENDERS, default=None, blank=True, null=True
     )
     phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.OneToOneField("Address", on_delete=models.CASCADE, related_name="profile", null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
