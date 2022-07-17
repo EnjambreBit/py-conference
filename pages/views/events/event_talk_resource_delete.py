552f595b8dee93da976b87511ee3f3e66111d554
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-07-17 20:13:25 -0300

    agregadas vistas para modificar/borrar recursos de presentacion

---
+++ pages/views/events/event_talk_resource_delete.py
@@ -0,0 +1,17 @@
+from django.views.generic import DeleteView
+from conferences.models.resources import Resource
+from django.urls import reverse_lazy
+
+
+class EventTalkResourceDeleteView(DeleteView):
+    template_name = "event/event-talk-resource-delete.html"
+    model = Resource
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["talk"] = self.get_object().talk
+        return context_data
+    
+    def get_success_url(self):
+        print("xxx", self.get_object().talk)
+        return reverse_lazy("talk_preview", kwargs={ "pk": self.get_object().talk.id })
--- pages/views/events/event_talk_resource_update.py
+++ pages/views/events/event_talk_resource_update.py
@@ -1,20 +1,41 @@
 from django.views.generic import UpdateView
 from django.urls import reverse_lazy
 from conferences.models.resources import Resource
-from conferences.models.talks import Talk
+from pages.forms.resources import ResourceLinkForm, ResourceFileForm, ResourceImageForm
+from django import forms
+
 
 class EventTalkResourceUpdateView(UpdateView):
     template_name = "event/event-talk-resource-form.html"
     model = Resource
-    success_url = reverse_lazy("talk_preview")
 
-    # def get(self, request, pk, *args, **kwargs):
-    #     context_data = self.get_context_data()
-    #     talk = Talk.objects.get(pk=pk)
-    #     context_data["talk"] = talk
+    # # def get(self, request, pk, *args, **kwargs):
+    # #     context_data = self.get_context_data()
+    # #     talk = Talk.objects.get(pk=pk)
+    # #     context_data["talk"] = talk
 
     def get_form_class(self):
-        component = self.get_object()
+        resource = self.get_object()
+        forms_class = {
+            "ResourceLink": ResourceLinkForm,
+            "ResourceFile": ResourceFileForm,
+            "ResourceImage": ResourceImageForm
+        }
+        return forms_class.get(resource.type, ResourceLinkForm)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["talk"].widget = forms.HiddenInput()
+        return form
+
+    # # def get_object(self, queryset=None):
+    # #     pk = self.kwargs.get("pk")
+    # #     resource = Resource.objects.get(id=pk)
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["talk"] = self.get_object().talk
+        return context_data
 
     def get_success_url(self):
-        return reverse_lazy("talk_preview", kwargs={"pk": self.get_object().id})
+        return reverse_lazy("talk_preview", kwargs={ "pk": self.model.talk.id })
