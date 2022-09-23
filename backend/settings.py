e937549084db306206c56a20c2c899070de490b7
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-09-23 11:59:53 -0300

    agregado filtro por fecha para fecha de charlas

--- backend/settings.py
+++ backend/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "import_export",
     "qr_code",
+    "rangefilter",
     "conferences",
     "pages",
 ]
--- conferences/admin_class/talk_rooms.py
+++ conferences/admin_class/talk_rooms.py
@@ -1,6 +1,16 @@
-from django.contrib import admin
-from conferences.models.talk_rooms import TalkRoom
 from conferences.filters.dropdown_filter import RelatedDropdownFilter
+from conferences.models.talk_rooms import TalkRoom
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportMixin
+from import_export.fields import Field
+from import_export.formats import base_formats
+from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter
+
+
+class TalkRoomResource(resources.ModelResource):
+    pass
+
 
 
 class TalkRoomAdmin(admin.ModelAdmin):
@@ -21,4 +31,5 @@ class TalkRoomAdmin(admin.ModelAdmin):
     list_filter = (
         ("talk", RelatedDropdownFilter),
         ("room", RelatedDropdownFilter),
+        ("date", DateRangeFilter),
     )
